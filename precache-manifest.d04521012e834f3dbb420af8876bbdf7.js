fd2bad5657fb01d1891f2e18f8f11be48f3315cf
Ula Dutchak <ulana.dutchak@gmail.com>
2021-08-30 15:21:19 +0300

    Updates

---
+++ precache-manifest.d04521012e834f3dbb420af8876bbdf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b0a88d5d4625d3b8cd85707fc59989",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75326220a4004bd51592",
+    "url": "./static/css/main.68fff2fd.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75326220a4004bd51592",
+    "url": "./static/js/main.5b83f0ee.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
